fcb2422171c92d965b9dbc2744b344cc25e7590f
cadenkarras16 <caden.karras4@gmail.com>
2022-03-02 19:25:31 -0700

    Mission 9 pt 1

--- CadenBookStore/Startup.cs
+++ CadenBookStore/Startup.cs
@@ -44,6 +44,10 @@ namespace CadenBookStore
             services.AddDistributedMemoryCache();
 
             services.AddSession();
+
+            services.AddScoped<Basket>(x => SessionBasket.GetBasket(x));
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
