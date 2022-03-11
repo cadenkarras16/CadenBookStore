e729b39c1855b61f02dbace5720a2ae7ebed7381
cadenkarras16 <caden.karras4@gmail.com>
2022-03-10 22:03:56 -0700

    Up until Video 8.
    Start on video 8 tomorrow.

--- CadenBookStore/Models/IBookProjectRepository.cs
+++ CadenBookStore/Models/IBookProjectRepository.cs
@@ -7,5 +7,10 @@ namespace CadenBookStore.Models
     {
         IQueryable<Book> Books { get; }
 
+        public void SaveBook(Book b);
+
+        public void CreateBook(Book b);
+
+        public void DeleteBook(Book b);
     }
 }
